c92289e0521be5827b554eeb7582b8ea761bb14b
Bruno Bitencourt Luiz <brunowdev@gmail.com>
2016-12-18 22:04:54 -0200

    Iniciando repositório com infinite-scroll normal e reverso

---
+++ infinite-scroll/public/js/app.js
@@ -0,0 +1,11 @@
+angular.module('sampleApp', [
+    'ngRoute',
+    'appRoutes',
+    'MainCtrl',
+    'NerdCtrl',
+    'NerdService',
+    'GeekCtrl',
+    'GeekService',
+    'infinite-scroll',
+    'zInfiniteScroll',
+    'CommentCtrl']);
